ceb2a82cea0e938235e436fc006f88d174170ed1
Ehsan Akhgari <ehsan@mozilla.com>
2017-07-25 23:22:43 -0400

    Bug 1377999 Part 2: Refactor the out of line free list code into a self-contained class; r=mats

--- xpcom/ds/nsArenaList.h
+++ xpcom/ds/nsArenaList.h
@@ -27,19 +27,7 @@ class nsArenaList
 {
 public:
   nsArenaList() = default;
-  ~nsArenaList()
-  {
-#if defined(MOZ_HAVE_MEM_CHECKS)
-    for (FreeList* entry = mFreeLists; entry != mozilla::ArrayEnd(mFreeLists); ++entry) {
-      nsTArray<void*>::index_type len;
-      while ((len = entry->mEntries.Length())) {
-        void* result = entry->mEntries.ElementAt(len - 1);
-        entry->mEntries.RemoveElementAt(len - 1);
-        MOZ_MAKE_MEM_UNDEFINED(result, entry->mEntrySize);
-      }
-    }
-#endif
-  }
+  ~nsArenaList() = default;
 
   /**
    * Pool allocation with recycler lists indexed by object-type ID (see above).
@@ -65,56 +53,12 @@ protected:
 
     FreeList* list = &mFreeLists[aCode];
 
-    nsTArray<void*>::index_type len = list->mEntries.Length();
-    if (list->mEntrySize == 0) {
-      MOZ_ASSERT(len == 0, "list with entries but no recorded size");
-      list->mEntrySize = aSize;
-    } else {
-      MOZ_ASSERT(list->mEntrySize == aSize,
-                 "different sizes for same object type code");
-    }
-
-    void* result;
-    if (len > 0) {
-      // Remove from the end of the mEntries array to avoid memmoving entries,
-      // and use SetLengthAndRetainStorage to avoid a lot of malloc/free
-      // from ShrinkCapacity on smaller sizes.  500 pointers means the malloc size
-      // for the array is 4096 bytes or more on a 64-bit system.  The next smaller
-      // size is 2048 (with jemalloc), which we consider not worth compacting.
-      result = list->mEntries.ElementAt(len - 1);
-      if (list->mEntries.Capacity() > 500) {
-        list->mEntries.RemoveElementAt(len - 1);
-      } else {
-        list->mEntries.SetLengthAndRetainStorage(len - 1);
-      }
-#if defined(DEBUG)
-      MOZ_MAKE_MEM_DEFINED(result, list->mEntrySize);
-      if (EnablePoisoning) {
-        char* p = reinterpret_cast<char*>(result);
-        char* limit = p + list->mEntrySize;
-        for (; p < limit; p += sizeof(uintptr_t)) {
-          uintptr_t val = *reinterpret_cast<uintptr_t*>(p);
-          if (val != mozPoisonValue()) {
-            MOZ_ReportAssertionFailure(
-              nsPrintfCString("nsArenaList: poison overwritten; "
-                              "wanted %.16" PRIx64 " "
-                              "found %.16" PRIx64 " "
-                              "errors in bits %.16" PRIx64 " ",
-                              uint64_t(mozPoisonValue()),
-                              uint64_t(val),
-                              uint64_t(mozPoisonValue() ^ val)).get(),
-              __FILE__, __LINE__);
-            MOZ_CRASH();
-          }
-        }
-      }
-#endif
-      MOZ_MAKE_MEM_UNDEFINED(result, list->mEntrySize);
+    if (void* result = list->GetFreeEntry(aSize)) {
       return result;
     }
 
     // Allocate a new chunk from the arena
-    list->mEntriesEverAllocated++;
+    list->NoteAllocated();
     return mPool.Allocate(aSize);
   }
   void Free(uint32_t aCode, void* aPtr)
@@ -123,20 +67,20 @@ protected:
 
     // Try to recycle this entry.
     FreeList* list = &mFreeLists[aCode];
-    MOZ_ASSERT(list->mEntrySize > 0, "object of this type was never allocated");
+    MOZ_ASSERT(list->EntrySize() > 0, "object of this type was never allocated");
 
     if (EnablePoisoning) {
-      mozWritePoison(aPtr, list->mEntrySize);
+      mozWritePoison(aPtr, list->EntrySize());
     }
 
-    MOZ_MAKE_MEM_NOACCESS(aPtr, list->mEntrySize);
-    list->mEntries.AppendElement(aPtr);
+    MOZ_MAKE_MEM_NOACCESS(aPtr, list->EntrySize());
+    list->Add(aPtr);
   }
 
   class FreeList
   {
-  public:
     nsTArray<void *> mEntries;
+  public:
     size_t mEntrySize;
     size_t mEntriesEverAllocated;
 
@@ -144,6 +88,83 @@ protected:
       : mEntrySize(0)
       , mEntriesEverAllocated(0)
     {}
+
+    ~FreeList()
+    {
+#if defined(MOZ_HAVE_MEM_CHECKS)
+      nsTArray<void*>::index_type len;
+      while ((len = mEntries.Length())) {
+        void* result = mEntries.ElementAt(len - 1);
+        mEntries.RemoveElementAt(len - 1);
+        MOZ_MAKE_MEM_UNDEFINED(result, mEntrySize);
+      }
+#endif
+    }
+
+    size_t EntrySize() const { return mEntrySize; }
+
+    void Add(void* aPtr)
+    {
+      mEntries.AppendElement(aPtr);
+    }
+
+    void NoteAllocated()
+    {
+      mEntriesEverAllocated++;
+    }
+
+    // Returns non-null if the free list contains a recylcled entry.
+    void* GetFreeEntry(size_t aSize)
+    {
+      nsTArray<void*>::index_type len = mEntries.Length();
+      if (mEntrySize == 0) {
+        MOZ_ASSERT(len == 0, "list with entries but no recorded size");
+        mEntrySize = aSize;
+      } else {
+        MOZ_ASSERT(mEntrySize == aSize,
+                   "different sizes for same object type code");
+      }
+
+      void* result;
+      if (len > 0) {
+        // Remove from the end of the mEntries array to avoid memmoving entries,
+        // and use SetLengthAndRetainStorage to avoid a lot of malloc/free
+        // from ShrinkCapacity on smaller sizes.  500 pointers means the malloc size
+        // for the array is 4096 bytes or more on a 64-bit system.  The next smaller
+        // size is 2048 (with jemalloc), which we consider not worth compacting.
+        result = mEntries.ElementAt(len - 1);
+        if (mEntries.Capacity() > 500) {
+          mEntries.RemoveElementAt(len - 1);
+        } else {
+          mEntries.SetLengthAndRetainStorage(len - 1);
+        }
+#if defined(DEBUG)
+        MOZ_MAKE_MEM_DEFINED(result, mEntrySize);
+        if (EnablePoisoning) {
+          char* p = reinterpret_cast<char*>(result);
+          char* limit = p + mEntrySize;
+          for (; p < limit; p += sizeof(uintptr_t)) {
+            uintptr_t val = *reinterpret_cast<uintptr_t*>(p);
+            if (val != mozPoisonValue()) {
+              MOZ_ReportAssertionFailure(
+                nsPrintfCString("nsArenaList: poison overwritten; "
+                                "wanted %.16" PRIx64 " "
+                                "found %.16" PRIx64 " "
+                                "errors in bits %.16" PRIx64 " ",
+                                uint64_t(mozPoisonValue()),
+                                uint64_t(val),
+                                uint64_t(mozPoisonValue() ^ val)).get(),
+                __FILE__, __LINE__);
+              MOZ_CRASH();
+            }
+          }
+        }
+#endif
+        MOZ_MAKE_MEM_UNDEFINED(result, mEntrySize);
+        return result;
+      }
+      return nullptr;
+    }
 
     size_t SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const
     { return mEntries.ShallowSizeOfExcludingThis(aMallocSizeOf); }
